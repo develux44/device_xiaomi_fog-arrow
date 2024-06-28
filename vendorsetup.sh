90d60a641902bcb5b650b6f3ea8b9cba09ebfea8
Iverz <iverzasterix@gmail.com>
2024-06-28 09:43:20 +0000

    fog: ArrowOS Bring Up!
    Signed-off-by: Iverz <iverzasterix@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,14 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone -b thirteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b 4.19 https://github.com/asterixiverz/kernel_xiaomi_fog-msm4.19 kernel/xiaomi/fog
 
-# Clone LineageOS' hardware/xiaomi
-git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# Clone hardware/xiaomi from @asterixiverz (Forking from PixelOS-AOSP)
+git clone -b thirteen-aosp https://github.com/asterixiverz/hardware_xiaomi hardware/xiaomi
+
+# Clang Missing Fixups
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/tejas101k/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 
 # Kernel fixups
 cd kernel/xiaomi/fog
