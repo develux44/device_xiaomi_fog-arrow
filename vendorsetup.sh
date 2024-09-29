85bfe5adb6d6329ce4b0f957fce6717e0ab2b99f
Iverz <140925089+asterixiverz@users.noreply.github.com>
2024-09-29 15:57:39 +0700

    vendorsetup: add warning while cloning device stuff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,12 +2,15 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
+echo -e "\e[1;33mwarning:\e[0m \e[1mClone Vendor Tree\e[0m"
 git clone -b fourteen https://github.com/asterixiverz/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
+echo -e "\e[1;33mwarning:\e[0m \e[1mClone Kernel Tree\e[0m"
 git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
+echo -e "\e[1;33mwarning:\e[0m \e[1mClone Hardware Dependencies for Xiaomi Devices from LineageOS\e[0m"
 git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
